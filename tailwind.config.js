f6640613b3970045c9dd33fc1005202d4e441bc0
Achun1130 <zx255235@gmail.com>
2023-03-25 01:15:09 +0800

    feat: UI 初版

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'muted': {
+          100: '#AFAFAF',
+          200: '#5A5A5A'
+        },
+        'primary': {
+          100: '#DB4444',
+          200: '#C6393e'
+        }
+      }
+    },
   },
   plugins: [],
 }
